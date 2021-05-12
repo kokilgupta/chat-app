d444317916a6f5ba203955cbbf457cd3aad7e07e
KOKIL GUPTA <kokilgupta682@gmail.com>
2021-05-12 13:49:19 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import {ChatEngine} from "react-chat-engine";
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 const App=()=>{
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return(
      <ChatEngine
          height="100vh"
          projectID="642d8fd9-bf05-4729-a8a5-511b77599019"
-         userName="Kokil"
-         userSecret="12345"
+         userName={localStorage.getItem('username')}
+         userSecret={localStorage.getItem('password')}
          renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
      />
   );
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '642d8fd9-bf05-4729-a8a5-511b77599019';
 
 const Modal = () => {
   const [username, setUsername] = useState('');
